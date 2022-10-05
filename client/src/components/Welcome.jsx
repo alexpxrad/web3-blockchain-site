ccb01848544765a347c92ba1616a2182f904d747
alexpxrad <alexpxrad@gmail.com>
2022-10-05 16:40:47 -0400

    adding address bar section

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -6,6 +6,13 @@ import { Loader } from './';
 
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'
 
+const Input = ({ placeholder }) => (
+    <input  
+        placeholder={placeholder}
+    /> 
+)
+;
+
 const Welcome = () => {
 
     const connectWallet = () => {
@@ -48,8 +55,31 @@ const Welcome = () => {
                     </div>
                 </div>
 
-                <div>
-                    
+                <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10' >
+                    <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'>
+                        <div className='flex justify-between flex-col w-full h-full' >
+                            <div className='flex justify-between flex-col w-full h-full'>
+                                <div className='flex justify-between items-start'>
+                                    <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
+                                        <SiEthereum fontSize={21} color="#fff" />
+                                    </div>
+                                    <BsInfoCircle fontSize={17} color='#fff'/>
+                                </div>
+                                <div>
+                                    <p className='text-white font-light text-sm'> 
+                                            Address
+                                    </p>
+                                    <p className='text-white font-semibold text-lg mt-1'> 
+                                            Ethereum
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                            <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
+                                <Input  placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
+                            </div>
                 </div>
 
             </div>
